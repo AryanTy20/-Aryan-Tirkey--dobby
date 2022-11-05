71b90100096f1e4b081f3f94b3ce1630ccae5835
Aryan Tirkey <65565216+AryanTy20@users.noreply.github.com>
2022-11-05 17:01:28 +0530

    Update axios.tsx

--- front-end/src/axios.tsx
+++ front-end/src/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+const baseURL = "https://dobby-assignment.herokuapp.com/api";
 
 export const customAxios = axios.create({
   baseURL,
