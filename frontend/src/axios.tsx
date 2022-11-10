59484258cdb7101381ddef8dd19832bc28f7cfb4
Aryan Tirkey <65565216+AryanTy20@users.noreply.github.com>
2022-11-10 19:04:59 +0530

    Add files via upload

---
+++ frontend/src/axios.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:5000/api";
+
+export const customAxios = axios.create({
+  baseURL,
+  withCredentials: true,
+  headers: {
+    "Content-type": "application/json",
+  },
+});
---
+++ frontend/src/layout/protectedRoute.tsx
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useUserContext } from "../hook/useUserContext";
+
+const protectedRoute = () => {
+  const { user, setUser } = useUserContext();
+
+  return user ? <Outlet /> : <Navigate to={"/login"} replace={true} />;
+};
+
+export default protectedRoute;
